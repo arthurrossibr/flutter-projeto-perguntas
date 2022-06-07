3b3675fb8994abad31551aabb509499e518920ee
arthurrossicardoso <arthurrossi15@gmail.com>
2022-06-07 15:42:46 -0300

    Adicionando botão na aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,41 @@ main() {
 
 class PerguntasApp extends StatelessWidget {
 
+  var perguntaSelecionada = 0;
+
+  void responder(){
+    perguntaSelecionada++;
+    print('Resposta $perguntaSelecionada');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+      'Qual é o seu instrutor favorito?'
+    ];
+
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Mundo'),
-      ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+              ),
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      )
     );
   }
 }
