f046a81c3d06ae51d0f877ade9f76a9433aa4d69
arthurrossicardoso <arthurrossi15@gmail.com>
2022-06-07 21:56:18 -0300

    Adicionando container no widget questao

--- lib/questao.dart
+++ lib/questao.dart
@@ -7,6 +7,14 @@ class Questao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+          texto,
+          style: const TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+      ),
+    );
   }
 }
