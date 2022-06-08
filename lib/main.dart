aa0fb6077b3d23d9a11fdbc868855bd87443476d
arthurrossicardoso <arthurrossi15@gmail.com>
2022-06-07 22:33:14 -0300

    Passando as perguntas para Maps

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class PerguntasApp extends StatefulWidget {
 class _PerguntasAppState extends State<PerguntasApp> {
   var _perguntaSelecionada = 0;
 
-  void _responder(){
+  void _responder() {
     setState(() {
       _perguntaSelecionada++;
     });
@@ -24,27 +24,34 @@ class _PerguntasAppState extends State<PerguntasApp> {
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-      'Qual é o seu instrutor favorito?'
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'resposta': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'resposta': ['Cachorro', 'Gato', 'Furão', 'Hamster'],
+      },
+      {
+        'texto': 'Qual é o seu veículo favorito?',
+        'resposta': ['Carro', 'Moto', 'Barco', 'Bicicleta'],
+      }
     ];
 
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Perguntas'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
-          ],
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Perguntas'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+          Resposta('Resposta 1', _responder),
+          Resposta('Resposta 2', _responder),
+          Resposta('Resposta 3', _responder),
+        ],
+      ),
+    ));
   }
 }
